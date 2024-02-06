9e9b13fe232821c6be8bf81bd85b886e6a281bb0
MistiXe <37303853+MistiXe@users.noreply.github.com>
2024-02-06 16:52:13 +0100

    Commit 4
    Création du Canvas

--- Modèle.py
+++ Modèle.py
@@ -1,7 +1,10 @@
 from Vue import * 
 from tkinter import * 
 
+
 root =  Tk()
+root.geometry("1200x600")
+créerGrille(root)
 root.mainloop()
 
 
--- Vue.py
+++ Vue.py
@@ -1,2 +1,8 @@
 from Controller import *
+from tkinter import *
 
+def créerGrille(root):
+    c = Canvas(root, width = 600, height= 600, bg = 'ivory')
+    
+    c.pack()
+    
