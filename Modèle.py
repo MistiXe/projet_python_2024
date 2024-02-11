07f274918c10b150e66309920fec136fa669a415
MistiXe <37303853+MistiXe@users.noreply.github.com>
2024-02-11 12:40:14 +0100

    Commit 6
    Ajout des pièces

--- Modèle.py
+++ Modèle.py
@@ -5,6 +5,9 @@ from tkinter import *
 root =  Tk()
 root.geometry("1210x610")
 créerGrille(root)
+generateZoneJ1(root)
+generateZoneJ2(root)
+
 root.mainloop()
 
 
--- Vue.py
+++ Vue.py
@@ -1,6 +1,11 @@
 from Controller import *
 from tkinter import *
 
+
+
+old=[None, None]
+
+
 def créerGrille(root):
     c = Canvas(root, width = 620, height= 620)
     x = 5
@@ -12,6 +17,47 @@ def créerGrille(root):
         y=y+45
         x=5        
     c.pack(anchor=CENTER, expand=False)
+
+
+def generateZoneJ1(root):
+    c = Canvas(root, width = 280,height = 610, background='ivory')
+    c.place(y=50)
+    c.create_text(50,40, font=40,text = "Joueur A", anchor=CENTER, fill='black')
+    générerPion(c)
+    
+   
+
+
+def generateZoneJ2(root):
+    c = Canvas(root, width = 280,height = 610, background='green')
+    c.create_text(50,40, font=40,text = "Joueur B", anchor=CENTER, fill='white')  
+    c.place(x = 930,y=50)
+   
+   
+
+
+def générerPion(cnv_pion):
+       
+     a = cnv_pion.create_rectangle(25,100,100,175, outline= 'white', width=4, fill='black', tags="pion")
+     cnv_pion.tag_bind("pion", "<Button-1>", cliquePion)
+     
+
+  
+     
+    
+     
+     
+    
+     
+     
+     
+     
+
+def cliquePion(event):
+    old[0] = event.x
+    old[1] = event.y
+    print(event.x)
+    
 
 
 
