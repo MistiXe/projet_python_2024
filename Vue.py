cf61ab3e4fbb28721273a0736b28be2d669a8c58
MistiXe <37303853+MistiXe@users.noreply.github.com>
2024-03-18 19:26:06 +0100

    C14
    REWORK

--- Vue.py
+++ Vue.py
@@ -1,95 +1,223 @@
 from Controller import *
 from tkinter import *
-from Variables import *
+from Variables import*
+
 
 
 old=[None, None]
 global c 
 c= 0
 
+matrice_rouge = [1]*144
+
+
+matrice_rouge[0] = 2
+matrice_rouge[132] = 2
+
+matrice_bleu = [1]*144
+matrice_bleu[11] = 2
+matrice_bleu[143] = 2
+
+def click(event):
+    global platforme
+    global selected
+    global dimensions
+    global player
+    global origins
+    global laliste
+    player = 0
+    
+    x,y = event.x , event.y
+
+    selecteds = platforme.find_overlapping(x,y,x,y)
+    if selecteds :
+        selected = selecteds[0]
+        cord = platforme.coords(selected)
+        origins = cord
+        w = cord[2] - cord[0]
+        h = cord[3] - cord[1]
+        dimensions = [w, h]
+        
+        blocs = laliste 
+
+        if selected in blocs[0]:
+           player = 1
+
+        elif selected in blocs[1]:
+           player = 2
+
+
+
+def move(event):
+    global selected
+    global dimensions
+
+    x,y = event.x , event.y
+
+    if selected:
+       platforme.coords(selected,x,y,x+dimensions[0],y+dimensions[1])
+
+def verifier(x,y,player):
+    global selected , platforme , dimensions , grille , matrice_bleu , matrice_bleu , matrice_rouge ,  checked , surface
+    matrice = []
+    if player == 1 :
+        matrice = matrice_rouge
+    
+    elif player == 2:
+        matrice = matrice_bleu
+
+
+    surface = [x,y ,x+dimensions[0] , y + dimensions[1]]
+    i =0
+    check1 = True
+    check2 = False
+    checked = []
+    while i<len(grille) and check1 == True:
+        e = grille[i]
+        
+     
+        if e[0]>= surface[0] and e[2]<= surface[2] and e[1]>=surface[1] and e[3] <= surface[3]:
+            checked.append(i)
+            if matrice[i] == 0:
+                check1 = False
+
+            if matrice[i] == 2:
+                check2 = True
+
+    
+        i = i+1
+        
+
+
+    return check1 and check2
+
+
+
+
+    
+
+def deposer(event):
+    global  selected , grille , platforme , dimensions , player , checked , surface , player , matrice_bleu , matrice_rouge
+    x,y = event.x , event.y
+    
+
+    matrice = []
+    if player == 1 :
+        matrice = matrice_rouge
+    
+    elif player == 2:
+        matrice = matrice_bleu
+
+    
+ 
+    for e in grille:
+        if x >= e[0] and x<= e[2] and y>=e[1] and y <= e[3]:
+            check = verifier(e[0],e[1], player)
+            if check :
+               platforme.coords(selected, e[0],e[1],e[0]+ dimensions[0] , e[1] + dimensions[1])
+               for i in checked:
+                   matrice_rouge[i] = 0
+                   matrice_bleu[i] = 0
+
+                   cord_grille = grille[i]
+
+                   if cord_grille[0] == surface[0] or  cord_grille[1] == surface[1] or cord_grille[2] == surface[2] or cord_grille[3] == surface[3] :
+                       if i > 11 :
+                           matrice[i-12] = 0
+
+                       if i < 132 :
+                           matrice[i+12] = 0
+
+                       if i % 12 != 0  :
+                           matrice[i-1] = 0
+
+                       if i% 12 != 11 :
+                           matrice[i+1] = 0
+
+                        
+                       if cord_grille[0] == surface[0] and   cord_grille[1] == surface[1] and i % 12 != 0  and i > 11:
+                           matrice[i-13] = 2
+
+                       if cord_grille[2] == surface[2] and   cord_grille[1] == surface[1] and i % 12 != 11 and i > 11 :
+                           matrice[i-11] = 2
+
+                       if cord_grille[0] == surface[0] and   cord_grille[3] == surface[3] and i % 12 != 0  and  i < 132:
+                           matrice[i+11] = 2
+
+                       if cord_grille[2] == surface[2] and   cord_grille[3] == surface[3] and i % 12 != 11   and  i < 132:
+                           matrice[i+13] = 2
+
+                           
+               if player == 1:
+                   matrice_rouge = matrice
+               elif player == 2:
+                   matrice_bleu = matrice 
+               selected = None
+                        
+               
+
+
+            else :
+                platforme.coords(selected,origins)
+                selected = None
+
+
+
+def rotate(event):
+    global selected , platforme , dimensions
+    x,y = event.x , event.y
+
+
+    if selected:
+        temp = dimensions[0]
+        dimensions[0]= dimensions[1]
+        dimensions[1] = temp
+        platforme.coords(selected,  x , y , x+dimensions[0] , y + dimensions[1])
+
+
+          
+
+
+
 def creerJeu(root):
+    global platforme
     global laliste
     global liste_bouge
-    global lagrille_theorique
     liste_bouge = []
-    platforme = Canvas(root, width=1200 , height=650)
-
+    global grille
+    platforme = Canvas(root, width=1600 , height=1000)
+    grille = []
     for i in range(12):
-        x =   400 +30*i 
+        
+        y = 160 + 30*i
         for j in range(12):
-            y = 160 + 30*j
+            x =   400 +30*j
             carré = platforme.create_rectangle(x,y,x+30,y+30)
-
-    platforme.pack()
-    laliste = genererPion(platforme)
-    lagrille_theorique = genererGrille()
-    
-    platforme.bind("<Button-1>", lambda event:cliquePion(event, platforme, laliste))
-    
-    
-def cliquePion(event, cnv, laliste):
-    global c
-    c = (c+1)%2
-    recta  = 0
-    for l in laliste:
-        for p in l:
-            cood_p = cnv.coords(p)
-            if(cood_p[0] <= event.x <= cood_p[0] + cood_p[2] and cood_p[1] <= event.y <= cood_p[1] + cood_p[3]):
-                recta = p
-                
-               
-    if(recta != 0 and recta not in liste_bouge):  
-        if(c == 1):
-            old[0] = event.x
-            old[1] = event.y
-            cnv.bind("<Motion>", lambda event:move(event, cnv, recta))
-        else:
-            cnv.unbind("<Motion>")
-            deposer(event.x,event.y,cnv, recta)
-    
-        
-    
-    
-def move(event, can, rect):
-    
-    x1, y1, x2, y2 = can.coords(rect)
-    move = False
-    can.move(rect, event.x - x1, event.y - y1)
-
-def deposer(x,y,can , rect):
-    x1, y1, x2, y2= can.coords(rect)
-    move = False
-    
-
-    for i in range(12):
-        for j in range(12):
-            if (400 +30*i  <= x<= 400+(i+1)*30 and 160 +30*j <= y <= 160+(j+1)*30):
-                l_i = genererIndiceduPion(i,j,1,4)
-                if(collision(l_i, lagrille_theorique) == False):
-                    print(majGrille(lagrille_theorique, l_i))
-                    can.move(rect,400+i*30-x1,160+j*30-y1)
-                    move = True
-                    liste_bouge.append(rect)
-            
- 
-
-   
-        
-
-    
-
+            grille.append(platforme.coords(carré))
 
            
 
-                
-                
+    platforme.pack()
+    laliste = genererPion(platforme)
     
-    
+
+  
+    for i in laliste:
+        for j in i :
+             platforme.tag_bind(j,"<Button-1>", click )
+
    
-    
-    
-    
-    
+    platforme.bind("<Button1-Motion>", move)
+    platforme.bind("<ButtonRelease-1>" , deposer)
+    platforme.bind("<Button-3>" , rotate) 
+
+
+
+
+
+
+
+
   
         
 
