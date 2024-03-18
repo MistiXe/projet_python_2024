e41a41f567d2d47f6b852311c0b4891482e57c24
MistiXe <37303853+MistiXe@users.noreply.github.com>
2024-03-18 09:44:54 +0100

    c12
    Collisions

--- Controller.py
+++ Controller.py
@@ -2,8 +2,8 @@ global tab_grille
 tab_grille= []
 LEN_JEU = 12
 
-def genererGrille(LEN):
-    for i in range(LEN):
+def genererGrille():
+    for i in range(LEN_JEU):
         tab_grille.append([])
         for j in range(LEN_JEU):
             tab_grille[i].append(0)
@@ -14,20 +14,18 @@ def majGrille(L, listeindice):
     for i in range(len(L)):
         for j in range(len(L)):
             for k in range(len(listeindice)):
-                    if(i == listeindice[k][0] and j == listeindice[k][1]):
-                        i1 = listeindice[k][0]
-                        i2 = listeindice[k][1]
+                    if(i == listeindice[0] and j == listeindice[1]):
+                        i1 = listeindice[0]
+                        i2 = listeindice[1]
                         L[i1][i2] = 1
     return L
 
 def collision(x,y, L):
-    if(L[x][y] == 1 or (L[x-1][y] == 1 ) or (L[x+1][y] == 1 ) or (L[x][y+1] == 1 )or (L[x][y-1] == 1) ):
+    if(L[x][y] == 1):
         return True
     else:
         return False
      
 
 
-L= genererGrille(12)
-l_ind = [[0,0], [0,1],[0,2]]
-print(majGrille(L, l_ind))
+
--- Vue.py
+++ Vue.py
@@ -10,9 +10,11 @@ c= 0
 
 
 
+
 def creerJeu(root):
     global laliste
     global liste_bouge
+    global lagrille_theorique
     liste_bouge = []
     platforme = Canvas(root, width=1200 , height=650)
 
@@ -24,7 +26,7 @@ def creerJeu(root):
 
     platforme.pack()
     laliste = genererPion(platforme)
-    genererGrille(12)
+    lagrille_theorique = genererGrille()
     
     platforme.bind("<Button-1>", lambda event:cliquePion(event, platforme, laliste))
     
@@ -38,7 +40,8 @@ def cliquePion(event, cnv, laliste):
             cood_p = cnv.coords(p)
             if(cood_p[0] <= event.x <= cood_p[0] + cood_p[2] and cood_p[1] <= event.y <= cood_p[1] + cood_p[3]):
                 recta = p
-            
+                x1, y1, x2, y2 = cnv.coords(recta)
+               
     if(recta != 0 and recta not in liste_bouge):  
         if(c == 1):
             old[0] = event.x
@@ -52,26 +55,31 @@ def cliquePion(event, cnv, laliste):
     
     
 def move(event, can, rect):
-   
+    
     x1, y1, x2, y2 = can.coords(rect)
     move = False
     can.move(rect, event.x - x1, event.y - y1)
 
 def deposer(x,y,can , rect):
-    
     x1, y1, x2, y2= can.coords(rect)
-    coord_last = can.coords(rect)
     move = False
-
+   
 
     for i in range(12):
         for j in range(12):
-            if (400 +30*i  <= x<= 400+(i+1)*30 and 160 +30*j <= y <= 160+(j+1)*30):
+            if (400 +30*i  <= x<= 400+(i+1)*30 and 160 +30*j <= y <= 160+(j+1)*30 and collision(i,j, lagrille_theorique ) == False):
+                liste_ind = [i,j]
+                majGrille(lagrille_theorique, liste_ind)
+                
                 can.move(rect,400+i*30-x1,160+j*30-y1)
                 move = True
                 liste_bouge.append(rect)
-                
-            
+                print(lagrille_theorique)
+ 
+
+   
+        
+
     
 
 
